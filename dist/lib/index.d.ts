5cec0ac0d5448182d98bb8cf97bcbea6856869b3
Ronald3217 <ronald3217@gmail.com>
2024-01-22 22:22:08 -0600

    UPDATE: TSDoc added to Interfaces

--- dist/lib/index.d.ts
+++ dist/lib/index.d.ts
@@ -1,20 +1,59 @@
 import * as React from 'react';
 import { FirebaseStorage, StorageError } from 'firebase/storage';
 
+/**
+ * Represents the configuration object for firebase-file-uploader-react custom hook.
+ */
 interface ConfigInterface {
+    /**
+     * Represents the Firebase storage instance.
+     */
     storage: FirebaseStorage;
+    /**
+     * Specifies the path for the storage location.
+     */
     path: string;
+    /**
+     * Optional boolean flag to include file extensions.
+     */
     includeExt?: boolean;
+    /**
+     * Optional parameter to specify the filename or a function to generate the filename.
+     */
     filename?: string | ((filename: string) => string);
 }
 interface HookValues {
+    /**
+    * Represents if the file is uploading.
+    */
     uploading: boolean;
+    /**
+     * Represents the progress of the file upload.
+     */
     progress: number;
+    /**
+     * Represents the URL of the uploaded file.
+     */
     fileURL: string;
+    /**
+     * Represents the name of the uploaded file.
+     */
     fileName: string;
+    /**
+     * Represents the type of the uploaded file.
+     */
     fileType: string;
+    /**
+     * Represents the original name of the file before upload.
+     */
     originalFileName: string;
+    /**
+     * Represents any error that occurred during file storage, or a boolean value indicating success.
+     */
     error: StorageError | boolean;
+    /**
+     * Represents the React component for the file uploader UI.
+     */
     FileUploaderUI: React.ForwardRefExoticComponent<InputProps & React.RefAttributes<HTMLInputElement>>;
 }
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
--- src/lib/hooks/useFirebaseFileUploader.tsx
+++ src/lib/hooks/useFirebaseFileUploader.tsx
@@ -7,20 +7,59 @@ export interface getFileName {
   payload?: string | ((filename: string) => string),
   file: File,
 }
+/**
+ * Represents the configuration object for firebase-file-uploader-react custom hook.
+ */
 export interface ConfigInterface {
+  /**
+   * Represents the Firebase storage instance.
+   */
   storage: FirebaseStorage,
+  /**
+   * Specifies the path for the storage location.
+   */
   path: string,
+  /**
+   * Optional boolean flag to include file extensions.
+   */
   includeExt?: boolean
+  /**
+   * Optional parameter to specify the filename or a function to generate the filename.
+   */
   filename?: string | ((filename: string) => string),
 }
 export interface HookValues {
+  /** 
+  * Represents if the file is uploading.
+  */
   uploading: boolean,
+  /**
+   * Represents the progress of the file upload.
+   */
   progress: number,
+  /**
+   * Represents the URL of the uploaded file.
+   */
   fileURL: string,
+  /**
+   * Represents the name of the uploaded file.
+   */
   fileName: string,
+  /**
+   * Represents the type of the uploaded file.
+   */
   fileType: string,
+  /**
+   * Represents the original name of the file before upload.
+   */
   originalFileName: string,
+  /**
+   * Represents any error that occurred during file storage, or a boolean value indicating success.
+   */
   error: StorageError | boolean,
+  /**
+   * Represents the React component for the file uploader UI.
+   */
   FileUploaderUI: React.ForwardRefExoticComponent<InputProps & React.RefAttributes<HTMLInputElement>>;
 }
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
@@ -52,7 +91,7 @@ const useFirebaseFileUploader = (config: ConfigInterface): HookValues => {
   const [originalFileName, setOriginalFileName] = React.useState<string>("");
   const [error, setError] = React.useState<StorageError | boolean>(false);
 
-  const Input:React.ForwardRefRenderFunction<HTMLInputElement, InputProps> = (props, ref) => {
+  const Input: React.ForwardRefRenderFunction<HTMLInputElement, InputProps> = (props, ref) => {
     // Funcion para subir la imagen
     const handleUploadStart = (snapshot: UploadTaskSnapshot) => {
       const progress = Math.round(
